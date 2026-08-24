4af628f15344e233eab1462c1e35ffcf9492a758
Sascha Karnatz <sascha@blish.cloud>
2026-08-24 15:35:23 +0200

    Add privilege and category method to permission sets
    All permission sets should have a privilege and a category method, to allow the permission sets seeding in Solidus. Otherwise the user will see a NotImplementedError during the seeding.
    Use the product category, because the Solidus::Review is integrated into the product view and it make sense to keep them in sync.

--- app/models/spree/permission_sets/review_management.rb
+++ app/models/spree/permission_sets/review_management.rb
@@ -3,6 +3,16 @@
 module Spree
   module PermissionSets
     class ReviewManagement < PermissionSets::Base
+      class << self
+        def privilege
+          :management
+        end
+
+        def category
+          :product
+        end
+      end
+
       def activate!
         can :manage, Spree::Review
       end
---
+++ spec/models/spree/permission_sets/review_display_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+require "cancan/matchers"
+require "spree/testing_support/dummy_ability"
+
+RSpec.describe Spree::PermissionSets::ReviewDisplay do
+  let(:ability) { DummyAbility.new }
+
+  subject { ability }
+
+  it { expect(described_class.privilege).to eq(:display) }
+  it { expect(described_class.category).to eq(:product) }
+
+  context "when activated" do
+    before { described_class.new(ability).activate! }
+
+    it { is_expected.to be_able_to(:display, Spree::Review) }
+    it { is_expected.to be_able_to(:admin, Spree::Review) }
+
+    it { is_expected.not_to be_able_to(:update, Spree::Review) }
+    it { is_expected.not_to be_able_to(:destroy, Spree::Review) }
+  end
+
+  context "when not activated" do
+    it { is_expected.not_to be_able_to(:display, Spree::Review) }
+    it { is_expected.not_to be_able_to(:admin, Spree::Review) }
+  end
+end
